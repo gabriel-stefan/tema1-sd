80d133c97af4942d77ee563e384ea3d3ffa684a2
Gabriel <gabrielstefan04@gmail.com>
2024-03-27 20:32:07 +0200

    modificari main

--- main.cpp
+++ main.cpp
@@ -4,6 +4,7 @@
 #include <chrono>
 #include "number_generator.h"
 #include "merge_sort.h"
+#include "verif.h"
 
 using namespace std;
 
@@ -13,13 +14,26 @@ int main()
 {
     int tests;
     fin >> tests;
-    for(int test = 1; test < tests; ++test)
+    for(int test = 1; test <= tests; ++test)
     {
         long long int n, max;
         fin >> n >> max;
 
+        generator(n, max);
+        ifstream in("test.txt");
+
+        vector<int> nums(n), copy(n);
+        for(int i = 1; i <= n; ++i)
+        {
+            in >> nums[i];
+            copy[i] = nums[i];
+        }
+
         auto start = std::chrono::high_resolution_clock::now();
+        mergeSort(nums, 0, n);
         auto end = std::chrono::high_resolution_clock::now();
+        auto duration = std::chrono::duration_cast<std::chrono::milliseconds>(end - start);
+        cout << "Merge Sort || Timp executie: " << duration.count() << "ms || " << "Corect: "<< (verifSort(nums) ? "Da" : "Nu") << "\n";
 
     }
 
